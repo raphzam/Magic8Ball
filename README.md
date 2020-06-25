5becf0b106002163d87c0c1b7040397869551792
raphzam <raphaeljasonzamora@gmail.com>
2020-06-25 16:03:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#Magic 8 Ball
+
+This program prompts the user for a question. The application will return one random response from the pool of Magic 8 Ball Responses.
+
+
+INPUT
+
+    a question
+    no to stop
+
+OUTPUT
+
+    one random answer from the list of responses
+
+PROCESS
+
+BEGIN
+
+    store all random answers to an array
+    
+    prompt user to ask a question
+    
+    reply to user with a random response
+
+END
---
+++ src/Magic8Ball.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Magic8Ball {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random  random = new Random();
+        String userQuestion;
+        String userContinue;
+
+        //store the answers
+        String [] magicResponses = {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+        int numOfResponses = magicResponses.length;
+//        System.out.println("How many possible responses: " + numOfResponses);
+
+
+        int responseNum = 0;// random.nextInt(numOfResponses);
+
+
+        //Prompt user until they quit
+        do {
+            responseNum = random.nextInt(numOfResponses);
+            System.out.println("Ask me a question: ");
+            userQuestion = input.nextLine();
+            System.out.println("YOU ASKED: " + userQuestion);
+            System.out.println("MAGIC 8 BALL SAYS: " + magicResponses[responseNum]);
+            System.out.println("Do you have another question for the Magic 8-Ball? (y/n)");
+            userContinue = input.nextLine();
+        } while (!userContinue.equalsIgnoreCase("n"));
+
+        System.out.println("Thank you for playing");
+
+
+    }
+
+}
+//
+//● It is certain
+//● "It is decidedly so"
+//● Without a doubt
+//● Yes definitely
+//● You may rely on it
+//● As I see it, yes
+//● Most likely
+//● Outlook good
+//● Yes
+//● Signs point to yes
+//● Reply hazy try again
+//● Ask again later
+//● Better not tell you now
+//● Cannot predict now
+//● Concentrate and ask again
+//● Don't count on it
+//● My reply is no
+//● My sources say no
+//● Outlook not so good
+//● Very doubtful
