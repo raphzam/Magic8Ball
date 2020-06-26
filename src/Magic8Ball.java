f85258010b225a050735fd2e64d7f45f5ac7fdba
raphzam <raphaeljasonzamora@gmail.com>
2020-06-25 23:13:08 -0400

    cleaned up the code

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -10,13 +10,15 @@ public class Magic8Ball {
         String userContinue;
 
         //store the answers
-        String [] magicResponses = {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+        String [] magicResponses = {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+                "You may rely on it", "As I see it, yes", "Most likely",
+                "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+                "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+                "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+
+
         int numOfResponses = magicResponses.length;
-//        System.out.println("How many possible responses: " + numOfResponses);
-
-
         int responseNum = 0;// random.nextInt(numOfResponses);
-
 
         //Prompt user until they quit
         do {
@@ -31,28 +33,10 @@ public class Magic8Ball {
 
         System.out.println("Thank you for playing");
 
-
     }
 
 }
-//
-//● It is certain
-//● "It is decidedly so"
-//● Without a doubt
-//● Yes definitely
-//● You may rely on it
-//● As I see it, yes
-//● Most likely
-//● Outlook good
-//● Yes
-//● Signs point to yes
-//● Reply hazy try again
-//● Ask again later
-//● Better not tell you now
-//● Cannot predict now
-//● Concentrate and ask again
-//● Don't count on it
-//● My reply is no
-//● My sources say no
-//● Outlook not so good
-//● Very doubtful
+
+
+
+
